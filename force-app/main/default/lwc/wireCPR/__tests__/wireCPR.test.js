a457ac535b0b1129c330129f5257274764ad781c
Lucas Valhos <lucas@valhos.com>
2020-07-05 14:41:26 -0300

    trailhead salesforce lwc test module

---
+++ force-app/main/default/lwc/wireCPR/__tests__/wireCPR.test.js
@@ -0,0 +1,44 @@
+import { createElement } from 'lwc';
+import WireCPR from 'c/wireCPR';
+import { CurrentPageReference } from 'lightning/navigation';
+import { registerTestWireAdapter } from '@salesforce/sfdx-lwc-jest';
+
+// mock realistic data
+const mockCurrentPageReference = require('./data/CurrentPageReference.json');
+
+// Register a stander test wire adapter.
+const currentPageReferenceAdapter = registerTestWireAdapter(CurrentPageReference);
+
+describe('c-wire-c-p-r', () => {
+
+  afterEach(() => {
+    while(document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+  });
+
+  it('renders the current page reference in <pre> tag', () => {
+
+    const element = createElement('c-wire-c-p-r', {
+      is: WireCPR
+    });
+
+    document.body.appendChild(element);
+
+    // Select element for validation
+    const preElement = element.shadowRoot.querySelector('pre');
+    expect(preElement).not.toBeNull();
+
+    // Emit data from @wire
+    currentPageReferenceAdapter.emit(mockCurrentPageReference);
+
+    return Promise.resolve().then(() => {
+      console.log(JSON.stringify(mockCurrentPageReference, null, 2));
+      expect(preElement.textContent).toBe(
+        JSON.stringify(mockCurrentPageReference, null, 2)
+      );
+    });
+
+  });
+
+});
